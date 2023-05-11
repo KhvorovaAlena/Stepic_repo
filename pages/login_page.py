c1ec29d95bd22fd87fb65b6b8e76f149f12907b4
Alena Khvorova <homichok66@gmail.com>
2023-05-11 18:57:44 +0400

    Add browser.current_url parameter for should_be_login_url method

--- pages/login_page.py
+++ pages/login_page.py
@@ -4,12 +4,11 @@ from .locators import LoginPageLocators
 
 class LoginPage(BasePage):
     def should_be_login_page(self):
-        self.should_be_login_url()
+        self.should_be_login_url(self.browser.current_url)
         self.should_be_login_form()
         self.should_be_register_form()
 
-    def should_be_login_url(self):
-        current_url = self.browser.current_url
+    def should_be_login_url(self, current_url):
         assert "login" in current_url, "This is not a Login page"
 
     def should_be_login_form(self):
