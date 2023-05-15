f71f9753a2c15a7904e1cdd816aeacc314938d86
Alena Khvorova <homichok66@gmail.com>
2023-05-15 17:39:37 +0400

    Add method for add products to the cart

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,13 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTRATION_FORM = (By.CSS_SELECTOR, "#register_form")
+
+
+class ProductPageLocators():
+    ADD_TO_CART_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main h1")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".price_color")
+    ALERT_WITH_PRODUCT_NAME = (By.XPATH, "//*[@id='messages']/div[1]")
+    ALERT_WITH_PRODUCT_PRICE = (By.XPATH, "//*[@id='messages']/div[3]")
+    PRODUCT_NAME_IN_CART = (By.CSS_SELECTOR, ".alert-success strong")
+    PRODUCT_PRICE_IN_CART = (By.CSS_SELECTOR, ".alert-info p strong")
---
+++ pages/product_page.py
@@ -0,0 +1,33 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class ProductPage(BasePage):
+    def add_item_in_cart(self):
+        add_to_cart_button = self.browser.find_element(*ProductPageLocators.ADD_TO_CART_BUTTON)
+        add_to_cart_button.click()
+
+    def check_product_price_and_name(self):
+        self.check_product_price()
+        self.check_product_name()
+
+    def check_product_price(self):
+        WebDriverWait(self.browser, 10).until(
+            EC.visibility_of_element_located(ProductPageLocators.ALERT_WITH_PRODUCT_PRICE))
+        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).get_attribute("textContent")
+        product_price_in_cart = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE_IN_CART).get_attribute("textContent")
+        assert product_price in product_price_in_cart, "Product price does not equal with cart price"
+        print(f"Product price {product_price}, product price in the cart {product_price_in_cart}")
+
+    def check_product_name(self):
+        WebDriverWait(self.browser, 10).until(
+            EC.visibility_of_element_located(ProductPageLocators.ALERT_WITH_PRODUCT_NAME))
+        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).get_attribute("textContent")
+        product_name_in_cart = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_IN_CART).get_attribute("textContent")
+        assert product_name in product_name_in_cart, "Product name does not equal with product name in the cart"
+        print(f"Product name {product_name}, product name in the cart {product_name_in_cart}")
+
+
+
